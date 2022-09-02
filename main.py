93439f0dcb91a2ff5bfc797dafd098bf133ab31e
Ramos <notpramdev@gmail.com>
2022-09-02 03:12:25 -0700

    tutorial code used from kivy website

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 import kivy
 kivy.require("2.1.0")
 from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class LoginScreen(GridLayout):
+    """
+    Login screen class used as root widget
+    """
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text="Username"))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text="Password"))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+        self.add_widget(Label(text="test text"))
+
+class MyApp(App):
+    """
+    My App
+    """
+    def build(self):
+        return LoginScreen()
+
+if __name__ == "__main__":
+    MyApp().run()
